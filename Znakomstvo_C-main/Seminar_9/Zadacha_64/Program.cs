9d2134a1c39b99ae10ff943a378fe8326f9aa632
Aleksander Sinkov <silwa2@yandex.ru>
2022-09-16 15:29:38 +0300

    Промежуток натуральных чисел M и N

---
+++ Znakomstvo_C-main/Seminar_9/Zadacha_64/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: Задайте значения M и N. 
+// Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.WriteLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.WriteLine());
+
+string PrintFromMtoN(int m, int n)
+{
+if (a > b)
+{
+if (a == b) return Convert.ToString(a);
+return a + " " + PrintFromMtoN(a - 1, b);
+}
+else
+{
+if (a == b) return Convert.ToString(a);
+return a + " " + PrintFromMtoN(a + 1, b);
+}
+}

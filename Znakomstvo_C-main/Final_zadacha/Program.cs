606d8a0bfc3eb801f16c685baf2a66f1eb19e1e8
Aleksander Sinkov <silwa2@yandex.ru>
2022-09-28 15:10:39 +0300

    Цыкл ввода строк

---
+++ Znakomstvo_C-main/Final_zadacha/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] AS; 
+int count; 
+string s;
+string[] AS2;
+
+Console.WriteLine("Enter strings:");
+
+count = 0; 
+AS = new string[count]; 
+
+do
+   {
+        s = Console.ReadLine();
+
+        if (s!="")
+    {
+    count++;
+
+    AS2 = new string[count];
+
+    for (int i = 0; i < AS2.Length - 1; i++)
+        AS2[i] = AS[i];
+        AS2[count - 1] = s;  
+        AS = AS2;
+    }
+    } 
+    while (s != "");
+
+ 
+
+
